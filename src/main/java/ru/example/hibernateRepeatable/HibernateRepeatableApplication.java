984b0842728eba5dd37d75a3993fde9bfc9a0306
Sibiryaq <4totakoeaaa@mail.ru>
2024-12-09 23:53:35 +0700

    1st commit

---
+++ src/main/java/ru/example/hibernateRepeatable/HibernateRepeatableApplication.java
@@ -0,0 +1,13 @@
+package ru.example.hibernateRepeatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRepeatableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRepeatableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/example/hibernateRepeatable/HibernateRepeatableApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.hibernateRepeatable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateRepeatableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
